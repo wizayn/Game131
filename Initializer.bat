0de9a16072ea129e1f13982ea330434d9bbbd421
username <email>
2018-02-08 19:27:56 -0600

    2/8 commit

--- Initializer.bat
+++ Initializer.bat
@@ -1,8 +1,54 @@
-@echo off
 
-git clone https://github.com/wizayn/Game131.git
 
-git config user.name wrobert8
+set firstVal=%~2
+set secondVal=%~4
+set thirdVal=%~6
 
-git config user.email wrobert8@stumail.jccc.edu
+IF "%1"=="username" goto userfirst
+IF "%1"=="email" goto emailfirst
+goto cloneRepo
 
+:userfirst
+set username=%~2
+IF"%3"=="email" (
+    set email=%~4
+    set RepoAddress=%~6
+)   else (
+        set cloneRepo=%~4
+        set email=$~6
+)
+goto display
+
+:emailfirst
+set email=%~2
+IF "%3"=="username" (
+    set username=%~4
+    set RepoAddress=%~6
+) else (
+    set RepoAddress=%~4
+    set username=%~6
+)
+goto display
+
+:cloneRepo
+set RepoAddress=%~2
+IF "%3"=="RepoAddress" (
+    set RepoAddress=%~4
+    set email=%~6
+)   else (
+        set email=%~4
+        set RepoAddress=$%~6
+)
+goto display
+
+:display
+echo username %username%
+echo email %email%
+echo Repo Address %RepoAddress%
+
+
+git clone RepoAddress
+
+git config user.name username
+
+git config user.email email
