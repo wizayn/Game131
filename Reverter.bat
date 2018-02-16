ac27747111f1b997739b60505337f2e06f2a9d74
svc-student <svc-student@student.directory.jccc>
2018-02-15 19:01:40 -0600

    Commit

--- Reverter.bat
+++ Reverter.bat
@@ -1,3 +1,9 @@
 @echo off
 
-git reset --hard "%~1"
+echo You are about to revert all changes to working directory since last commit. Y to continue, N to cancel
+
+set /p input=Y or N
+IF %input%=="Y"
+git reset --hard
+IF %input%=="N"
+EXIT /B
