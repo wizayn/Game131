08260fc1835d0dc5eb43401cc4f7ebc9371ed373
svc-student <svc-student@student.directory.jccc>
2018-02-15 19:18:00 -0600

    Final Commit

---
+++ Help.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo To use Committer do: Committer.bat "Commit message here"
+
+echo To use the Initializer do: Initializer.bat username "your username here" email "your email address here" RepoAddress "The repository address here"
+
+echo To use the Reverter do: Reverter.bat Then enter Y to confirm that you wish to revert to last commit or N to cancel. 
+
+echo Updater is not completed. 
