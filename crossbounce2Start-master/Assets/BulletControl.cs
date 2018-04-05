7eac8a7049a536644f12eb9e74f1e51019a8a20c
wrobert8 <wrobert8@stumail.jccc.edu>
2018-04-05 16:45:47 -0500

    Added Project2

---
+++ crossbounce2Start-master/Assets/BulletControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+    public float startForce = 1000;
+    public float minVelocityBeforeDestroy = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody.AddForce(transform.up * startForce);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (myRigidbody.velocity.magnitude < minVelocityBeforeDestroy)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
---
+++ crossbounce2Start-master/Assets/CrossbowUserControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossbowUserControl : MonoBehaviour {
+
+    public float rotationSpeed = 90f;
+    public GameObject bullet;
+
+    private Transform bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+        bulletSpawn = transform.Find("bulletSpawn");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Spawn a projectile
+            GameObject newBullet = GameObject.Instantiate(bullet, bulletSpawn);
+            newBullet.transform.parent = null;
+        }
+    }
+}
