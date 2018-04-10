24bc625431e84e01e50fc4643f503b8ab3953cca
wrobert8 <wrobert8@stumail.jccc.edu>
2018-04-10 17:43:30 -0500

    Worked on adding list of sprites that can be added to scene

--- crossbounce2Start-master/Assets/Editor/LevelEditorWindow.cs
+++ crossbounce2Start-master/Assets/Editor/LevelEditorWindow.cs
@@ -5,8 +5,30 @@ using UnityEditor;
 using System.Text;
 
 public class LevelEditorWindow : EditorWindow {
+    public int arrayTemp = 0;
+    public GameObject[] temp;
+    public string[] options;
+    public int index = 0;
+    
 
-    //only one instance of this private static variable
+    [MenuItem("Examples/Editor GUILayout Popup usage")]
+
+    
+    static void Init()
+    {
+        GameObject tempCamera = GameObject.Find("Main Camera");
+        ObjectOptionScript tempScript = tempCamera.GetComponent<ObjectOptionScript>();
+        for(int i = 0; i < tempScript  )
+        EditorWindow window = GetWindow(typeof(LevelEditorWindow));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        index = EditorGUILayout.Popup(index, options);
+            
+    }
+    ////only one instance of this private static variable
     private static LevelEditorWindow instance;
     public static void ShowWindow()
     {
@@ -14,48 +36,48 @@ public class LevelEditorWindow : EditorWindow {
         instance.titleContent = new GUIContent("Level Editor");
     }
 
-    private float currentX = 0.0f;
-    private void OnGUI()
-    {
-        GUILayout.Label("Hello world");
+    //private float currentX = 0.0f;
+    //private void OnGUI()
+    //{
+    //    GUILayout.Label("Hello world");
 
-        //if(GUILayout.Button("Create Cube"))
-        //{
-        //    string[] cubeGuids = AssetDatabase.FindAssets("crossbowRoot");
+    //    //if(GUILayout.Button("Create Cube"))
+    //    //{
+    //    //    string[] cubeGuids = AssetDatabase.FindAssets("crossbowRoot");
 
-        //    StringBuilder guidBuilder = new StringBuilder();
-        //    foreach(string cubeGuid in cubeGuids)
-        //    {
-        //        guidBuilder.AppendLine(cubeGuid);
+    //    //    StringBuilder guidBuilder = new StringBuilder();
+    //    //    foreach(string cubeGuid in cubeGuids)
+    //    //    {
+    //    //        guidBuilder.AppendLine(cubeGuid);
 
-        //    }
-        //    UnityEngine.MonoBehaviour.print(guidBuilder.ToString());
+    //    //    }
+    //    //    UnityEngine.MonoBehaviour.print(guidBuilder.ToString());
 
-        //    if(cubeGuids.Length >0)
-        //    {
-        //        string trueCubeGuid = cubeGuids[0];
+    //    //    if(cubeGuids.Length >0)
+    //    //    {
+    //    //        string trueCubeGuid = cubeGuids[0];
 
-        //        // 2. Get the asset's path from the GUID
-        //        string assetPath = AssetDatabase.GUIDToAssetPath(trueCubeGuid);
-        //        UnityEngine.MonoBehaviour.print(assetPath);
+    //    //        // 2. Get the asset's path from the GUID
+    //    //        string assetPath = AssetDatabase.GUIDToAssetPath(trueCubeGuid);
+    //    //        UnityEngine.MonoBehaviour.print(assetPath);
 
 
-        //        //3. Fetch the object
-        //        GameObject cubeTemplate = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
+    //    //        //3. Fetch the object
+    //    //        GameObject cubeTemplate = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
 
-        //        GameObject newCube = GameObject.Instantiate(cubeTemplate);
-        //        newCube.name = cubeTemplate.name;
+    //    //        GameObject newCube = GameObject.Instantiate(cubeTemplate);
+    //    //        newCube.name = cubeTemplate.name;
 
-        //        //Funsies = spawn in a line along x
-        //        Vector3 newCubePosition = newCube.transform.position;
-        //        newCubePosition.x = currentX;
-        //        newCube.transform.position = newCubePosition;
+    //    //        //Funsies = spawn in a line along x
+    //    //        Vector3 newCubePosition = newCube.transform.position;
+    //    //        newCubePosition.x = currentX;
+    //    //        newCube.transform.position = newCubePosition;
 
-        //        currentX += 1f;
+    //    //        currentX += 1f;
 
-        //    }
-        //}
-    }
+    //    //    }
+    //    //}
+    //}
 
 
 
